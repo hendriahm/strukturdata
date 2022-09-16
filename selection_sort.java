90356c48843be8019b7810b5643780eec2eda4db
hendriahm <hendri.chan@gmail.com>
2022-09-16 14:43:50 +0700

    Update selection_sort.java

--- selection_sort.java
+++ selection_sort.java
@@ -1,13 +1,8 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package bab_vii;
-
-/**
- *
  * @author Cipta Ramadhani
+ * Buku Dasar Algoritma dan Struktur Data Bahasa Java
  */
+
 public class selection_sort {
     
     public static int[] selec(int[] array)
