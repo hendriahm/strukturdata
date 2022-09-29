1483e9d04764c3ae34ba2881f19c5a0dd8f5fa99
hendriahm <hendri.chan@gmail.com>
2022-09-29 10:14:56 +0700

    Update insertion_sort.java

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,28 +1,36 @@
-/*
- * @author Cipta Ramadhani
- * Buku Dasar Algoritma dan Struktur Data Bahasa Java
- */
-
-public class insertion_sort {    
-    static int[] insertion(int[] array)
-    {
-     int ukuran=array.length;  
-     int max,j;
-     int i=1;
-     
-     while(i<ukuran)
-     {
-         max=array[i];
-         for(j=i;j>0;j--)
-         {
-             if(max<array[j-1])
-                 array[j]=array[j-1];
-             else
-                 break;
-         }         
-         array[j]=max;         
-         i++;
-     }     
-     return array;     
-    }    
+//source: https://www.guru99.com/
+public class InsertionSortDetail {
+public static void main(String a[]) {
+int[] myArray = {4, 3, 2, 10, 12, 1, 5, 6};
+System.out.println("Before Insertion Sort");
+printArray(myArray);
+//sorting array using insertion sort
+insertionSort(myArray);
+System.out.println("After Insertion Sort");
+printArray(myArray);
+}
+public static void insertionSort(int arr[]) {
+int n = arr.length;
+for (int i = 1; i < n; i++)
+{ System.out.println("Sorting yang ke: "+(i));
+int key = arr[i];
+int j = i-1;
+while ( (j > -1) && ( arr [j] > key ) )
+{
+System.out.println("Bandingkan "+ key + " dan " + arr [j]);
+arr [j+1] = arr [j];
+j--;
+}
+arr[j+1] = key;
+System.out.println("Tukarkan elemen: Array baru setelah tukar");
+printArray(arr);
+}
+}
+static void printArray(int[] array){
+for(int i=0; i < array.length; i++)
+{
+System.out.print(array[i] + " ");
+}
+System.out.println();
+}
 }
